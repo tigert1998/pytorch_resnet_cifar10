13c7bc03451647d07004f281bedc449e31eeb59a
Xiaohu Tang <tigertang.zju@qq.com>
2023-02-02 08:43:41 +0000

    Add in_channels argument

--- resnet.py
+++ resnet.py
@@ -90,11 +90,11 @@ class BasicBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=10):
+    def __init__(self, block, num_blocks, num_classes=10, in_channels=3):
         super(ResNet, self).__init__()
         self.in_planes = 16
 
-        self.conv1 = nn.Conv2d(3, 16, kernel_size=3,
+        self.conv1 = nn.Conv2d(in_channels, 16, kernel_size=3,
                                stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(16)
         self.layer1 = self._make_layer(block, 16, num_blocks[0], stride=1)
